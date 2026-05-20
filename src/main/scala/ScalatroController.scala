bd858581f6d77370455c4435d46fa549afb0649f
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-05-20 11:36:31 +0200

    research: switch between scenes

--- src/main/scala/ScalatroController.scala
+++ src/main/scala/ScalatroController.scala
@@ -1,7 +1,7 @@
 package scalatro
 
 import javafx.fxml.{FXML, Initializable}
-import javafx.scene.control.Label
+import javafx.scene.control.{Button, Label, TextField}
 
 import java.net.URL
 import java.util.ResourceBundle
@@ -9,6 +9,7 @@ import scala.compiletime.uninitialized
 
 class ScalatroController extends Initializable:
   @FXML private var label: Label = uninitialized
+  @FXML private var button: Button = uninitialized
 
   override def initialize(url: URL, rb: ResourceBundle): Unit =
     val javaVersion = System.getProperty("java.version")
@@ -16,3 +17,14 @@ class ScalatroController extends Initializable:
     label.setText(
       "Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + "."
     )
+
+    button.setOnMouseClicked(_ =>
+      SceneRouter.switchTo(button.getScene)("/scalatro/start.fxml")
+    )
+
+class StartController extends Initializable:
+  @FXML private var mirror: Label = uninitialized
+  @FXML private var input: TextField = uninitialized
+
+  override def initialize(url: URL, resourceBundle: ResourceBundle): Unit =
+    mirror.textProperty().bindBidirectional(input.textProperty())
--- src/main/scala/ScalatroMain.scala
+++ src/main/scala/ScalatroMain.scala
@@ -5,10 +5,9 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
 
-object ScalatroMain:
-  @main
-  def main(): Unit =
-    Application.launch(classOf[MainApp])
+@main
+def main(): Unit =
+  Application.launch(classOf[MainApp])
 
 class MainApp extends Application:
   override def start(stage: Stage): Unit =
@@ -20,4 +19,11 @@ class MainApp extends Application:
     scene.getStylesheets.add(cssUrl.toExternalForm)
     stage.setTitle("JavaFX and Gradle")
     stage.setScene(scene)
+
     stage.show()
+
+object SceneRouter:
+
+  def switchTo(scene: Scene)(fxml: String): Unit =
+    val root: Parent = FXMLLoader.load(getClass.getResource(fxml))
+    scene.setRoot(root)
