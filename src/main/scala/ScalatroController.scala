56191c485e58a34dcdbe7dfeea1fa1a50dd75cf1
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-05-20 11:57:04 +0200

    feat: creation of alpha version of the GUI

--- src/main/scala/ScalatroController.scala
+++ src/main/scala/ScalatroController.scala
@@ -8,19 +8,8 @@ import java.util.ResourceBundle
 import scala.compiletime.uninitialized
 
 class ScalatroController extends Initializable:
-  @FXML private var label: Label = uninitialized
-  @FXML private var button: Button = uninitialized
 
-  override def initialize(url: URL, rb: ResourceBundle): Unit =
-    val javaVersion = System.getProperty("java.version")
-    val javafxVersion = System.getProperty("javafx.version")
-    label.setText(
-      "Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + "."
-    )
-
-    button.setOnMouseClicked(_ =>
-      SceneRouter.switchTo(button.getScene)("/scalatro/start.fxml")
-    )
+  override def initialize(url: URL, rb: ResourceBundle): Unit = {}
 
 class StartController extends Initializable:
   @FXML private var mirror: Label = uninitialized
