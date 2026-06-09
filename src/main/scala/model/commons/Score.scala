6f7b45d589e95599bd008c347b65f8f6b6a8b121
Mattia Ronchi <mattia.ronchi5@studio.unibo.it>
2026-06-09 22:02:51 +0200

    feat: add Score

---
+++ src/main/scala/model/commons/Score.scala
@@ -0,0 +1,62 @@
+package scalatro
+package model.commons
+
+import model.commons.Chips.Chips
+import model.commons.Mult.Mult
+import model.commons.Score.Score
+
+object Chips:
+  type Chips = Integer
+
+  object Chips:
+    def apply(d: Integer): Chips =
+      require(d >= 0, "Chips must be positive")
+      d
+
+object Mult:
+  type Mult = Double
+
+  object Mult:
+    def apply(d: Double): Mult =
+      require(d >= 0.0, "Mult must be positive")
+      d
+
+trait HandScoreCalculator:
+  import Chips.Chips, Mult.Mult
+  import Score.Score
+  def calculate(chips: Chips, mult: Mult): Score
+
+object BasicHandScoreCalculator extends HandScoreCalculator:
+  override def calculate(chips: Chips, mult: Mult): Score = Score(chips * mult)
+
+object AvgSquaredHandScoreCalculator extends HandScoreCalculator:
+  override def calculate(chips: Chips, mult: Mult): Score =
+    val avg = (chips+mult)/2
+    Score(avg*avg)
+
+object HandScore:
+  import Chips.Chips, Mult.Mult
+
+  case class HandScore(chips: Chips, mult: Mult)
+
+  def apply(chips: Chips, mult: Mult): HandScore =
+    HandScore(chips, mult)
+
+object Score:
+  import HandScore.HandScore
+  opaque type Score = Double
+  def apply(d: Double): Score =
+    require(d >= 0.0, "Score must be positive")
+    d
+  def apply(using calculator : HandScoreCalculator)(handScore: HandScore) : Score =
+    calculator.calculate(handScore.chips, handScore.mult)
+  extension (s: Score)
+    def +(other: Score): Score = s + other
+//    def -(other: Score): Score = s - other
+    def *(other: Score): Score = s * other
+//    def >(other: Score): Boolean = s > other
+    def <(other: Score): Boolean = s < other
+    def >=(other: Score): Boolean = s >= other
+//    def <=(other: Score): Boolean = s >= other
+
+  val zero: Score = Score(0.0)
