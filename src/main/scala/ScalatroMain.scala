2ff5156e35f5b07c51477e7c194182ff0704efc9
Andrea Baldazzi <andreabaldazzi03@gmail.com>
2026-06-13 20:53:20 +0200

    feat: connect together View and Controller and launch program in a functional way

--- src/main/scala/ScalatroMain.scala
+++ src/main/scala/ScalatroMain.scala
@@ -1,27 +1,47 @@
 package scalatro
 
+import controller.SingleRoundController
+import model.round.RoundAction
+import view.{FxController, FxView}
+
+import cats.effect.IO
+import cats.effect.std.Queue
+import cats.effect.unsafe.implicits.global
 import javafx.fxml.FXMLLoader
 import javafx.scene.{Parent, Scene}
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
 
 @main
-def main(): Unit =
-  MainApp.main(Array())
+def main(): Unit = MainApp.main(Array())
 
 object MainApp extends JFXApp3:
   override def start(): Unit =
     this.stage = PrimaryStage()
     val fxmlUrl = getClass.getResource("/scalatro/scene.fxml")
     val cssUrl = getClass.getResource("/scalatro/styles.css")
+    val loader = new FXMLLoader(fxmlUrl)
+    val root: Parent = loader.load()
 
-    val root: Parent = FXMLLoader.load(fxmlUrl)
     val scene = Scene(root)
     scene.getStylesheets.add(cssUrl.toExternalForm)
     stage.setTitle("ScalaFX and SBT")
     stage.setScene(scene)
-
     stage.show()
+
+    val fxController: FxController = loader.getController
+
+    // Functional program bootstrapping
+    val program: IO[Unit] =
+      for
+        queue <- Queue.unbounded[IO, RoundAction]
+        view = FxView(fxController, queue) // injects queue into fxController
+        ctrl = SingleRoundController(view, queue)
+        _ <- ctrl.start()
+      yield ()
+
+    // This instruction must be the last one, since we are at top-level entry-point
+    program.unsafeRunAndForget()
 
 object SceneRouter:
 
---
+++ src/main/scala/view/FxController.scala
@@ -0,0 +1,143 @@
+package scalatro
+package view
+
+import model.commons.{Card, Deck, Rank}
+import model.round.{Round, RoundAction}
+
+import cats.effect.IO
+import cats.effect.std.Queue
+import cats.effect.unsafe.implicits.global
+import javafx.application.Platform
+import javafx.fxml.{FXML, Initializable}
+import javafx.scene.control.{Button, Label, TextField, ToggleButton}
+import javafx.scene.image.{Image, ImageView}
+import scalafx.scene.control.{Label as SfxLabel, TextField as SfxTextField}
+
+import java.net.URL
+import java.util.ResourceBundle
+import scala.compiletime.uninitialized
+
+@SuppressWarnings(Array("org.wartremover.warts.Null"))
+class FxController extends Initializable:
+
+  // Card graphics
+  @FXML private var card1: ImageView = uninitialized
+  @FXML private var card2: ImageView = uninitialized
+  @FXML private var card3: ImageView = uninitialized
+  @FXML private var card4: ImageView = uninitialized
+  @FXML private var card5: ImageView = uninitialized
+  @FXML private var card6: ImageView = uninitialized
+  @FXML private var card7: ImageView = uninitialized
+  @FXML private var card8: ImageView = uninitialized
+
+  // Card toggle buttons
+  @FXML private var cardBtn1: ToggleButton = uninitialized
+  @FXML private var cardBtn2: ToggleButton = uninitialized
+  @FXML private var cardBtn3: ToggleButton = uninitialized
+  @FXML private var cardBtn4: ToggleButton = uninitialized
+  @FXML private var cardBtn5: ToggleButton = uninitialized
+  @FXML private var cardBtn6: ToggleButton = uninitialized
+  @FXML private var cardBtn7: ToggleButton = uninitialized
+  @FXML private var cardBtn8: ToggleButton = uninitialized
+
+  // Info labels
+  @FXML private var roundScoreLabel: Label = uninitialized
+  @FXML private var goalLabel: Label = uninitialized
+  @FXML private var chipsLabel: Label = uninitialized
+  @FXML private var multLabel: Label = uninitialized
+  @FXML private var handLabel: Label = uninitialized
+  @FXML private var handLevelLabel: Label = uninitialized
+  @FXML private var deckLabel: Label = uninitialized
+  @FXML private var handsRemainingLabel: Label = uninitialized
+  @FXML private var discardsRemainingLabel: Label = uninitialized
+
+  // Action buttons
+  @FXML private var playButton: Button = uninitialized
+  @FXML private var discardButton: Button = uninitialized
+
+  // Convenient grouped lists (order matters – matches FXML fx:id numbering)
+  private def cardViews: List[ImageView] =
+    List(card1, card2, card3, card4, card5, card6, card7, card8)
+
+  private def cardButtons: List[ToggleButton] = List(
+    cardBtn1,
+    cardBtn2,
+    cardBtn3,
+    cardBtn4,
+    cardBtn5,
+    cardBtn6,
+    cardBtn7,
+    cardBtn8
+  )
+
+  private var actionQueue: Option[Queue[IO, RoundAction]] = None
+
+  private var handSlots: List[(ToggleButton, Card)] = List()
+
+  override def initialize(url: URL, rb: ResourceBundle): Unit =
+    playButton.setOnAction(_ => onPlay())
+    discardButton.setOnAction(_ => onDiscard())
+
+  /** Inject the action queue. Must be called the first render. */
+  def setActionQueue(queue: Queue[IO, RoundAction]): Unit =
+    actionQueue = Some(queue)
+
+  /** Update all UI nodes to reflect the new Round state. */
+  def update(round: Round): Unit =
+    Platform.runLater { () =>
+      // Scores
+      roundScoreLabel.setText(round.score.asDouble.toString)
+      deckLabel.setText(s"${round.deck.size} left")
+
+      // Reset all card slots, then repopulate from the current hand
+      cardViews.foreach(_.setImage(null))
+      cardButtons.foreach { btn =>
+        btn.setSelected(false)
+        btn.setVisible(false)
+      }
+
+      handSlots = round.hand.toList.zip(cardButtons).map { case (card, btn) =>
+        setCardImage(cardViews(cardButtons.indexOf(btn)), card)
+        btn.setVisible(true)
+        (btn, card)
+      }
+    }
+
+  private def selectedCards: Seq[Card] =
+    handSlots.collect { case (btn, card) if btn.isSelected => card }
+
+  private def onPlay(): Unit =
+    actionQueue.foreach(
+      _.offer(RoundAction.PlayCards(selectedCards)).unsafeRunAndForget()
+    )
+
+  private def onDiscard(): Unit =
+    actionQueue.foreach(
+      _.offer(RoundAction.DiscardCards(selectedCards)).unsafeRunAndForget()
+    )
+
+  private def setCardImage(imageView: ImageView, card: Card): Unit =
+    val rankString = card.rank match
+      case Rank.Two   => "2"
+      case Rank.Three => "3"
+      case Rank.Four  => "4"
+      case Rank.Five  => "5"
+      case Rank.Six   => "6"
+      case Rank.Seven => "7"
+      case Rank.Eight => "8"
+      case Rank.Nine  => "9"
+      case Rank.Ten   => "10"
+      case other      => other
+    val imagePath = s"/scalatro/cards/${rankString}_of_${card.suit}.png"
+    val image = new Image(getClass.getResourceAsStream(imagePath))
+    imageView.setImage(image)
+
+@SuppressWarnings(Array("org.wartremover.warts.Null"))
+class StartController extends Initializable:
+  @FXML private var mirror: Label = uninitialized
+  @FXML private var input: TextField = uninitialized
+
+  override def initialize(url: URL, resourceBundle: ResourceBundle): Unit =
+    val sfxMirror = new SfxLabel(mirror)
+    val sfxInput = new SfxTextField(input)
+    sfxMirror.text <== sfxInput.text
