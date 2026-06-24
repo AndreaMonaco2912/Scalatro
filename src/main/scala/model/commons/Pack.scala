0481f95c16258dfb0bb482f1eb53231f15466ab9
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-06-24 14:28:59 +0200

    feat: add Joker pack and solve todos

--- src/main/scala/model/commons/Pack.scala
+++ src/main/scala/model/commons/Pack.scala
@@ -6,14 +6,24 @@ import scala.util.Random
 case class Pack[A](items: Seq[A])
 
 trait PackFactory[A]:
+  private val SMALL_PACK_SIZE = 3
+  private val BIG_PACK_SIZE = 5
   def pool: Seq[A]
   def apply(n: Int)(using rng: Random): Pack[A] =
     require(n >= 0, s"cannot present a pack with a negative amount of cards")
     Pack(rng.shuffle(pool).take(n))
-  def apply(n: Int, externalPool: Seq[A])(using rng: Random): Pack[A] =//TODO a black list would be better then a white list (pool - externalPool)
+  def apply(n: Int, blackList: Seq[A])(using rng: Random): Pack[A] =
     require(n >= 0, s"cannot present a pack with a negative amount of cards")
-    Pack(rng.shuffle(externalPool).take(n))
-  //TODO: add a factory for a small pack with 3 items
+    Pack(rng.shuffle(pool diff blackList).take(n))
+
+  def smallPack(using rng: Random): Pack[A] =
+    Pack(rng.shuffle(pool).take(SMALL_PACK_SIZE))
+  def smallPack(blackList: Seq[A])(using rng: Random): Pack[A] =
+    Pack(rng.shuffle(pool diff blackList).take(SMALL_PACK_SIZE))
+  def bigPack(using rng: Random): Pack[A] =
+    Pack(rng.shuffle(pool).take(BIG_PACK_SIZE))
+  def bigPack(blackList: Seq[A])(using rng: Random): Pack[A] =
+    Pack(rng.shuffle(pool diff blackList).take(BIG_PACK_SIZE))
 
 object CardsPack extends PackFactory[Card]:
   val pool: Seq[Card] =
@@ -24,3 +34,6 @@ object CardsPack extends PackFactory[Card]:
 
 object PlanetPack extends PackFactory[Planet]:
   val pool: Seq[Planet] = Planet.values.toSeq
+
+object JokerPack extends PackFactory[JokerType]:
+  val pool: Seq[JokerType] = JokerType.values.toSeq
