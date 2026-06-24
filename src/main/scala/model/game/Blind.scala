861381ea1bff8d2cbf397238977db1a2e8dc13fb
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-06-24 11:57:33 +0200

    refactor: improve readability of Blind

--- src/main/scala/model/game/Blind.scala
+++ src/main/scala/model/game/Blind.scala
@@ -4,25 +4,14 @@ package model.game
 import model.commons.Score
 import model.commons.Score.Score
 
-case class Blind(
-    roundNum: Int,
-    targetScore: Score
-):
+case class Blind(roundNum: Int, targetScore: Score):
   def isBeaten(achieved: Score): Boolean = achieved >= targetScore
 
-  def next: Blind =
-    copy(
-      roundNum = roundNum + 1,
-      targetScore = targetScore * Score(Blind.increaseAmount)
-    )
+  def next: Blind = Blind(roundNum + 1, targetScore * Score(1.5))
 
 object Blind:
   val increaseAmount = 1.5
   val initialScore: Score = Score(300)
   private val initialRound = 1
 
-  def first: Blind =
-    Blind(
-      initialRound,
-      initialScore
-    )
+  def first: Blind = Blind(initialRound, initialScore)
