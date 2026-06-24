82bc4d89a22118f4b2465a048b4c825971a07745
Andrea Baldazzi <andreabaldazzi03@gmail.com>
2026-06-24 20:24:04 +0200

    test: improve RoundManagerSpec by mocking and testing the render function and checking the final action queue size

--- src/test/scala/model/round/RoundManagerSpec.scala
+++ src/test/scala/model/round/RoundManagerSpec.scala
@@ -10,11 +10,12 @@ import cats.effect.IO
 import cats.effect.std.Queue
 import cats.effect.unsafe.implicits.global
 import cats.syntax.all.*
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 /** A test spec for [[RoundManager]] */
-class RoundManagerSpec extends AnyFlatSpec with Matchers:
+class RoundManagerSpec extends AnyFlatSpec with Matchers with MockFactory:
 
   private val c1 = Card(Rank.Jack, Suit.Clubs)
   private val c2 = Card(Rank.Jack, Suit.Diamonds)
@@ -22,81 +23,132 @@ class RoundManagerSpec extends AnyFlatSpec with Matchers:
   private val c4 = Card(Rank.Queen, Suit.Hearts)
   private val c5 = Card(Rank.King, Suit.Clubs)
   private val c6 = Card(Rank.King, Suit.Diamonds)
+  private val c7 = Card(Rank.Five, Suit.Hearts)
 
   private val initialHand = Seq(c1, c2, c3, c4)
-  private val initialDeck = Deck(Seq(c5, c6))
+  private val initialDeck = Deck(Seq(c5, c6, c7))
 
   private val roundNum = 1
-  private val defaultGameState = GameState.initial
+  private val initialGameState = GameState.initial
+
+  private given ScoreConfig = ScoreConfig.default
 
   private def runSequence(
       initialRound: Round,
-      actions: Seq[RoundAction]
+      actions: Seq[RoundAction],
+      render: Round => IO[Unit] = _ => IO.unit
   ): Round =
     val testProgram = for
       queue <- Queue.unbounded[IO, RoundAction]
-      manager = RoundManager(_ => IO.unit, queue.take)
       _ <- actions.traverse_(action => queue.offer(action))
+      manager = RoundManager(render, queue.take)
       finalRound <- manager.startRound(initialRound)
-    yield finalRound
+      queueFinalSize <- queue.size
+    yield
+      queueFinalSize shouldBe 0 // all actions must have been consumed
+      finalRound
+
     testProgram.unsafeRunSync()
 
-  "A RoundManager" should "return immediately if the initial round is already finished" in:
-    val gameState = GameState.initial
-    val initialRound = Round(Score(300), initialHand, initialDeck, gameState)
-    val finalRound = runSequence(initialRound, Seq())
-    finalRound shouldBe initialRound
-
-  it should "apply a PlayCards action and finish the round if the score reaches the blind" in:
-    val score = Score(15.0)
-    val blind = Blind(roundNum, score)
-    val initialRound = Round(
+  private def roundWithBlindScore(targetScore: Score.Score): Round =
+    Round(
       Score.zero,
       initialHand,
       initialDeck,
-      defaultGameState.copy(blind = blind)
+      initialGameState.copy(blind = Blind(roundNum, targetScore))
     )
-    val actions = Seq(PlayCards(Seq(c1)))
-    val finalRound = runSequence(initialRound, actions)
-    finalRound.score shouldBe score
-    finalRound.hand should not contain c1
-    finalRound.isFinished shouldBe true
 
-  it should "accumulate score over multiple PlayCards actions until blind is beaten" in:
-    val score = Score(30.0)
-    val blind = Blind(roundNum, score)
+  private def assertRoundFinished(
+      round: Round,
+      expectedScore: Score.Score,
+      consumedCards: Card*
+  ): Unit =
+    round.score shouldBe expectedScore
+    consumedCards.foreach(card => round.hand should not contain card)
+    round.isFinished shouldBe true
+
+  private def mockRenderSequence(rounds: Round*): Round => IO[Unit] =
+    val render = mockFunction[Round, IO[Unit]]
+    inSequence:
+      rounds.foreach(round => render expects round returning IO.unit)
+    render
+
+  "A RoundManager" should "return immediately if the initial round is already finished, rendering only once" in:
     val initialRound = Round(
-      Score.zero,
+      initialGameState.blind.targetScore,
       initialHand,
       initialDeck,
-      defaultGameState.copy(blind = blind)
+      initialGameState
     )
-    val actions = Seq(
-      PlayCards(Seq(c1)),
-      PlayCards(Seq(c2))
-    )
-    val finalRound = runSequence(initialRound, actions)
-    finalRound.score shouldBe score
-    finalRound.hand should not contain c1
-    finalRound.hand should not contain c2
-    finalRound.isFinished shouldBe true
+    val render = mockRenderSequence(initialRound)
+    runSequence(initialRound, Seq(), render) shouldBe initialRound
 
-  it should "apply a DiscardCards action, drawing new cards, and then continue until PlayCards beats the blind" in:
-    val score = Score(15.0)
-    val blind = Blind(roundNum, score)
-    val initialRound = Round(
-      Score.zero,
-      initialHand,
-      initialDeck,
-      defaultGameState.copy(blind = blind)
+  private case class PlayCase(
+      description: String,
+      targetScore: Score.Score,
+      actions: Seq[RoundAction],
+      consumedCards: Seq[Card]
+  )
+
+  private val playCases = Seq(
+    PlayCase(
+      description =
+        "apply a PlayCards action and finish the round if the score reaches the blind",
+      targetScore = Score.calculateScore(Seq(c1)),
+      actions = Seq(PlayCards(Seq(c1))),
+      consumedCards = Seq(c1)
+    ),
+    PlayCase(
+      description =
+        "accumulate score over multiple PlayCards actions until blind is beaten",
+      targetScore =
+        Score.calculateScore(Seq(c1)) + Score.calculateScore(Seq(c2)),
+      actions = Seq(PlayCards(Seq(c1)), PlayCards(Seq(c2))),
+      consumedCards = Seq(c1, c2)
+    ),
+    PlayCase(
+      description =
+        "apply a DiscardCards action, drawing new cards, and then continue until PlayCards beats the blind",
+      targetScore = Score.calculateScore(Seq(c1, c2)),
+      actions = Seq(DiscardCards(Seq(c1)), PlayCards(Seq(c1, c2))),
+      consumedCards = Seq(c1, c2)
     )
-    val initialDeckSize = initialRound.deck.size
-    val actions = Seq(
-      DiscardCards(Seq(c1)),
-      PlayCards(Seq(c2))
+  )
+
+  playCases.foreach { playCase =>
+    it should playCase.description in:
+      val finalRound =
+        runSequence(roundWithBlindScore(playCase.targetScore), playCase.actions)
+      assertRoundFinished(
+        finalRound,
+        playCase.targetScore,
+        playCase.consumedCards*
+      )
+  }
+
+  it should "call render with the initial round, plus once per subsequent round values" in:
+    val firstScore = Score.calculateScore(Seq(c1))
+    val secondScore = firstScore + Score.calculateScore(Seq(c2))
+    val initialRound = roundWithBlindScore(secondScore)
+
+    val (drawnAfterFirst, deckAfterFirst) = initialRound.deck.draw(1)
+    val roundAfterFirstPlay = initialRound.modify(
+      score = firstScore,
+      hand = initialHand.filterNot(_ == c1) :++ drawnAfterFirst,
+      remainingPlays = initialRound.remainingPlays - 1,
+      deck = deckAfterFirst
     )
-    val finalRound = runSequence(initialRound, actions)
-    finalRound.score shouldBe score
-    finalRound.hand should not contain c1
-    finalRound.hand should not contain c2
-    finalRound.isFinished shouldBe true
+
+    val (drawnAfterSecond, deckAfterSecond) = roundAfterFirstPlay.deck.draw(1)
+    val finalRound = roundAfterFirstPlay.modify(
+      score = secondScore,
+      hand = roundAfterFirstPlay.hand.filterNot(_ == c2) :++ drawnAfterSecond,
+      remainingPlays = roundAfterFirstPlay.remainingPlays - 1,
+      deck = deckAfterSecond
+    )
+
+    val actions = Seq(PlayCards(Seq(c1)), PlayCards(Seq(c2)))
+    val render =
+      mockRenderSequence(initialRound, roundAfterFirstPlay, finalRound)
+
+    runSequence(initialRound, actions, render) shouldBe finalRound
