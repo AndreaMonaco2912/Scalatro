30f506de2af62e15b60e783835023c5b16d9c4ea
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-06-24 10:45:31 +0200

    feat: add default shop

---
+++ src/main/scala/model/shop/Shop.scala
@@ -0,0 +1,15 @@
+package scalatro
+package model.shop
+
+import model.commons.{Card, CardsPack, Pack, Planet, PlanetPack}
+
+import scala.util.Random
+
+case class Shop(cardPack: Pack[Card], planetPack: Pack[Planet])
+
+object Shop:
+  private val cardPackSize = 3
+  private val planetPackSize = 3
+
+  def default(using Random): Shop =
+    Shop(CardsPack(cardPackSize), PlanetPack(planetPackSize))
