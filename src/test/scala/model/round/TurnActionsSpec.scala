6431a3a5355e962b354d20d126ab661f1fdcbad8
Andrea Baldazzi <andreabaldazzi03@gmail.com>
2026-06-25 11:14:25 +0200

    test: improve TurnActionsSpec by mocking CardOrderer and ScoreConfig behaviors

--- src/test/scala/model/round/TurnActionsSpec.scala
+++ src/test/scala/model/round/TurnActionsSpec.scala
@@ -4,16 +4,13 @@ package model.round
 import model.commons.*
 import model.game.GameState
 
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 /** A test spec for [[TurnActions]] */
-class TurnActionsSpec extends AnyFlatSpec with Matchers:
+class TurnActionsSpec extends AnyFlatSpec with Matchers with MockFactory:
   import TurnActions.*
-
-  private given scoreConfig: ScoreConfig = ScoreConfig.default
-  private given calculator: HandScoreCalculator = scoreConfig.calculator
-  private given cardOrderer: CardOrderer = CardOrderer.sortByRank
 
   private val c1 = Card(Rank.Ace, Suit.Clubs)
   private val c2 = Card(Rank.Two, Suit.Hearts)
@@ -26,6 +23,24 @@ class TurnActionsSpec extends AnyFlatSpec with Matchers:
   private val initialDeck: Deck = Deck(Seq(c4, c5, c6))
   private val initialRound: Round =
     Round(Score.zero, initialHand, initialDeck, GameState.initial)
+  private val simulatedScore = Score(123456)
+
+  private def nUsagesScoreConfig(usages: Int): ScoreConfig =
+    val simulatedScore = Score(123456)
+    val scoreCalculator = mock[HandScoreCalculator]
+    scoreCalculator.calculate
+      .expects(*, *)
+      .returning(simulatedScore)
+      .repeated(usages)
+    ScoreConfig.default.copy(calculator = scoreCalculator)
+
+  private def nUsagesCardOrderer(expectedHand: Hand, usages: Int): CardOrderer =
+    val cardOrderer = mock[CardOrderer]
+    cardOrderer.order
+      .expects(expectedHand)
+      .returning(expectedHand.reverse)
+      .repeated(usages)
+    cardOrderer
 
   "discardCards" should "remove discarded cards and add drawn replacements to hand" in:
     val result = discardCards(Seq(c1)).runS(initialRound).value
@@ -40,23 +55,28 @@ class TurnActionsSpec extends AnyFlatSpec with Matchers:
     result.remainingDiscards shouldBe initialRound.remainingDiscards - 1
 
   "playCards" should "remove played cards and add drawn replacements to hand" in:
+    given ScoreConfig = nUsagesScoreConfig(1)
     val result = playCards(Seq(c1)).runS(initialRound).value
     result.hand shouldBe Seq(c2, c3, c4)
 
   it should "remove the drawn cards from the deck" in:
+    given ScoreConfig = nUsagesScoreConfig(1)
     val result = playCards(Seq(c1)).runS(initialRound).value
     result.deck shouldBe Deck(Seq(c5, c6))
 
   it should "decrease remaining plays by 1" in:
+    given ScoreConfig = nUsagesScoreConfig(1)
     val result = playCards(Seq(c1)).runS(initialRound).value
     result.remainingPlays shouldBe initialRound.remainingPlays - 1
 
   it should "increase the round score based on the played cards" in:
+    given ScoreConfig = nUsagesScoreConfig(2)
     val cardsToPlay = Seq(c1)
     val result = playCards(cardsToPlay).runS(initialRound).value
     result.score shouldBe initialRound.score
       + Score.calculateScore(cardsToPlay)
 
-  "orderCards" should "order the cards in hand using a given CardOrderer" in:
+  "orderCards" should "order the cards in hand using a the provided CardOrderer" in:
+    given cardOrderer: CardOrderer = nUsagesCardOrderer(initialHand, 2)
     val result = orderCards.runS(initialRound).value
     result.hand shouldBe cardOrderer.order(initialHand)
