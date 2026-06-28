930104f169f822787495b9f3d5250ba74ae0a54a
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-06-28 16:10:02 +0200

    feat: add new effects

--- src/main/scala/app/Msg.scala
+++ src/main/scala/app/Msg.scala
@@ -2,43 +2,53 @@ package scalatro
 package app
 
 import model.commons.{Card, CardOrderer, Joker, Planet}
+import model.commons.Score.Score
+import model.game.{Blind, GameState}
+import model.shop.Shop
+
+sealed trait Msg
 
 object Msg:
-  enum DefaultAction:
+  enum DefaultAction extends Msg:
     case Pause
 
   /** An action the user can perform during the round */
-  enum RoundAction:
+  enum RoundAction extends Msg:
     /** The action of playing a group of cards
-     * @param cards
-     *   the cards to play
-     */
+      * @param cards
+      *   the cards to play
+      */
     case PlayCards(cards: Seq[Card])
 
     /** The action of discarding a group of cards
-     * @param cards
-     *   the cards to discard
-     */
+      * @param cards
+      *   the cards to discard
+      */
     case DiscardCards(cards: Seq[Card])
 
     /** The action of ordering cards according to a specific [[CardOrderer]]
-     * @param orderer
-     *   the card orderer
-     */
+      * @param orderer
+      *   the card orderer
+      */
     case OrderHand(orderer: CardOrderer)
 
-  enum RoundEndAction:
+  enum RoundEndAction extends Msg:
     case NextRound
     case Restart
 
-  enum ShopAction:
+  enum ShopAction extends Msg:
     case OpenCardPack
     case OpenPlanetPack
     case OpenJokerPack
     case SkipShop
 
-  enum PackSelection:
+  enum PackSelection extends Msg:
     case SelectCard(card: Card)
     case SelectPlanet(planet: Planet)
     case SelectJoker(joker: Joker)
     case SkipPack
+
+  enum InternalEffect extends Msg:
+    case RoundWon(gameState: GameState)
+    case RoundLost(blind: Blind, finalScore: Score)
+    case ShopReady(gameState: GameState, shop: Shop)
