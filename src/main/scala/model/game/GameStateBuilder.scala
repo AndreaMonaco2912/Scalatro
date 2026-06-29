7c301bbf788bcd594248bc5d39a221f865b7d5e5
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-06-29 16:12:35 +0200

    refactor: removed useless annotations

--- src/main/scala/model/game/GameStateBuilder.scala
+++ src/main/scala/model/game/GameStateBuilder.scala
@@ -19,13 +19,13 @@ import scala.annotation.targetName
   * }}}
   */
 class GameStateBuilder:
-  var handSize: Int = GameState.initialHandSize
-  var remainingHands: Int = GameState.initialHandNum
-  var remainingDiscards: Int = GameState.initialDiscardNum
-  var deck: Deck = Deck()
-  var blind: Blind = Blind.first
-  var jokers: Seq[Joker] = Seq.empty
-  var levels: HandTypeLevels = HandTypeLevels.initial
+  private var handSize: Int = GameState.initialHandSize
+  private var remainingHands: Int = GameState.initialHandNum
+  private var remainingDiscards: Int = GameState.initialDiscardNum
+  private var deck: Deck = Deck()
+  private var blind: Blind = Blind.first
+  private var jokers: Seq[Joker] = Seq.empty
+  private var levels: HandTypeLevels = HandTypeLevels.initial
 
   def build: GameState = GameState(
     handInformation = HandInformation(
@@ -41,48 +41,39 @@ class GameStateBuilder:
 
 object GameStateBuilder:
   /** It creates a builder, makes it available in the implicit scope of the
-    * block, runs the block, and builds the state. *
+    * block, runs the block, and builds the GameState.
     */
   def configure(configuration: GameStateBuilder ?=> Unit): GameState =
     val builder = GameStateBuilder()
-    // By calling `configuration(using builder)`, we inject the builder
-    // into the context of everything inside the curly braces.
     configuration(using builder)
     builder.build
 
   object DSL:
 
     object HandSize:
-      @targetName("assign")
       infix def :=(value: Int)(using b: GameStateBuilder): Unit =
         b.handSize = value
 
     object Hands:
-      @targetName("assign")
       infix def :=(value: Int)(using b: GameStateBuilder): Unit =
         b.remainingHands = value
 
     object Discards:
-      @targetName("assign")
       infix def :=(value: Int)(using b: GameStateBuilder): Unit =
         b.remainingDiscards = value
 
     object DeckInGame:
-      @targetName("assign")
       infix def :=(deck: Deck)(using b: GameStateBuilder): Unit =
         b.deck = deck
 
     object BlindInGame:
-      @targetName("assign")
       infix def :=(blind: Blind)(using b: GameStateBuilder): Unit =
         b.blind = blind
 
     object Jokers:
-      @targetName("assign")
       infix def :=(jokers: Seq[Joker])(using b: GameStateBuilder): Unit =
         b.jokers = jokers
 
     object Levels:
-      @targetName("assign")
       infix def :=(levels: HandTypeLevels)(using b: GameStateBuilder): Unit =
         b.levels = levels
