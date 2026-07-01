ec4b17a66445caab670dcf8e040c16b213e9e0c0
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-07-01 22:21:15 +0200

    feat: use logic programming for calculate combinations in Hint.scala

--- src/main/scala/model/extra/Hint.scala
+++ src/main/scala/model/extra/Hint.scala
@@ -5,14 +5,26 @@ import model.commons.Score.{Score, calculateScore}
 import model.commons.{Card, ScoreConfig}
 import model.round.Hand
 
+import Scala2P.{*, given}
+import alice.tuprolog.{SolveInfo, Term}
+
 object Hint:
+
+  private val allCombinationsEngine: Term => LazyList[SolveInfo] =
+    mkPrologEngine("""
+    combos([], []).
+    combos([H|T],[H|T2]) :- combos(T,T2).
+    combos([_|T],T2) :- combos(T,T2).
+  """)
+
   def best(hand: Hand)(using ScoreConfig): Seq[Card] =
     require(hand.sizeIs > 0, "Hand must have at least one card")
     rankedPlays(hand).foldLeft(Option.empty[(Seq[Card], Score)]) { (acc, cur) =>
       (acc, cur) match
         case (None, best)                                 => Some(best)
         case (Some((bestHand, bestScore)), (hand, score)) =>
-          if score > bestScore then Some(cur) else acc
+          if score >= bestScore && hand.sizeIs < bestHand.size then Some(cur)
+          else acc
     } match
       case Some((bestHand, _)) => bestHand
       case None => throw new IllegalArgumentException("empty hand")
@@ -20,8 +32,23 @@ object Hint:
   private def rankedPlays(
       hand: Hand
   )(using ScoreConfig): LazyList[(Seq[Card], Score)] =
+    val indices: Term = hand.indices
+    val goal: Term = s"combos($indices, Combo)"
     for
-      n <- LazyList.range(1, math.min(hand.size, 5))
-      combo <- LazyList.from(hand.combinations(n))
+      solveInfo <- allCombinationsEngine(goal)
+      comboTerm = Scala2P.extractTerm(solveInfo, "Combo")
+//      _ = System.out.println(comboTerm)
+      comboIndices = comboTerm.toString
+        .stripPrefix("[")
+        .stripSuffix("]")
+        .split(",")
+        .filter(_.nonEmpty)
+        .map(_.trim.toInt)
+        .toSeq
+//      _ = System.out.println(comboIndices)
+      if comboIndices.nonEmpty && comboIndices.size <= 5
+      combo = comboIndices.map(hand)
+//      _ = System.out.println(combo)
       score = calculateScore(combo)
+//      _ = System.out.println(score)
     yield combo -> score
---
+++ src/main/scala/model/extra/Scala2P.scala
@@ -0,0 +1,55 @@
+package scalatro
+package model.extra
+import alice.tuprolog.*
+
+object Scala2P:
+  def extractTerm(solveInfo: SolveInfo, i: Integer): Term =
+    solveInfo.getSolution.asInstanceOf[Struct].getArg(i).getTerm
+
+  def extractTerm(solveInfo: SolveInfo, s: String): Term =
+    solveInfo.getTerm(s)
+
+  given Conversion[String, Term] = Term.createTerm(_)
+  given Conversion[Seq[_], Term] = _.mkString("[", ",", "]")
+  given Conversion[String, Theory] = Theory.parseWithStandardOperators(_)
+
+  def mkPrologEngine(theory: Theory): Term => LazyList[SolveInfo] =
+    val engine = Prolog()
+    engine.setTheory(theory)
+
+    goal =>
+      val solutions: Iterable[SolveInfo] = new Iterable[SolveInfo]:
+        override def iterator: Iterator[SolveInfo] = new Iterator[SolveInfo]:
+          var solution: Option[SolveInfo] = Some(engine.solve(goal))
+
+          override def hasNext: Boolean =
+            solution.fold(false)(si => si.isSuccess || si.hasOpenAlternatives)
+
+          override def next(): SolveInfo =
+            solution.fold(
+              throw new NoSuchElementException(
+                "next() called on exhausted iterator"
+              )
+            ) { current =>
+              solution =
+                if current.hasOpenAlternatives then Some(engine.solveNext())
+                else None
+              current
+            }
+
+      solutions.to(LazyList)
+
+  def solveWithSuccess(
+      engine: Term => LazyList[SolveInfo],
+      goal: Term
+  ): Boolean =
+    engine(goal).map(_.isSuccess).headOption.contains(true)
+
+  def solveOneAndGetTerm(
+      engine: Term => LazyList[SolveInfo],
+      goal: Term,
+      term: String
+  ): Term =
+    engine(goal).headOption.fold(
+      throw new NoSuchElementException(s"No solution found for goal: $goal")
+    )(extractTerm(_, term))
