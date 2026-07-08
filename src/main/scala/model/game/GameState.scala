b8706992ea6dab82ac7cb8257d247c28a3dcf816
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-07-09 01:22:37 +0200

    fix: now initial starts with first blind

--- src/main/scala/model/game/GameState.scala
+++ src/main/scala/model/game/GameState.scala
@@ -51,7 +51,7 @@ object GameState:
     GameState(
       initialHand,
       Deck(),
-      BlindProgression.first.next.next,
+      BlindProgression.first,
       Seq.empty,
       HandTypeLevels.initial,
       SelectionPolicies.default
