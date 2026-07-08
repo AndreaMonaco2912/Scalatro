ec433a1d8a5b37f7782ab5c89072f3d7c7949e68
Andrea Baldazzi <andreabaldazzi03@gmail.com>
2026-07-08 22:52:50 +0200

    feat: center view to screen and fix initial size

--- src/main/scala/ScalatroMain.scala
+++ src/main/scala/ScalatroMain.scala
@@ -1,12 +1,13 @@
 package scalatro
 
+import runtime.Runtime
+import view.{GameViews, Resources}
+
 import cats.effect.unsafe.implicits.global
 import javafx.scene.Scene
 import javafx.scene.layout.StackPane
 import scalafx.application.JFXApp3
 import scalafx.application.JFXApp3.PrimaryStage
-import runtime.Runtime
-import view.{GameViews, Resources}
 
 @main
 def main(): Unit = MainApp.main(Array())
@@ -15,10 +16,12 @@ object MainApp extends JFXApp3:
   override def start(): Unit =
     this.stage = PrimaryStage()
     val cssUrl = getClass.getResource(Resources.stylesheet)
-    val scene = Scene(new StackPane()) // placeholder root; first render swaps it
+    // placeholder root; first render swaps it
+    val scene = Scene(new StackPane(), 1400, 800)
     scene.getStylesheets.add(cssUrl.toExternalForm)
     stage.setTitle("Scalatro")
     stage.setScene(scene)
+    stage.centerOnScreen()
     stage.show()
 
     Runtime(GameViews(scene)).run.unsafeRunAndForget()
