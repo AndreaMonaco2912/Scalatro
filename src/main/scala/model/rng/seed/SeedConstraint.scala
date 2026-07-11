aaa5b4af3b6edf9b5d259b4a2e7a736a6ea4ab18
Andrea Baldazzi <andreabaldazzi03@gmail.com>
2026-07-11 19:24:24 +0200

    fix: seed search not working for some hand types

--- src/main/scala/model/rng/seed/SeedConstraint.scala
+++ src/main/scala/model/rng/seed/SeedConstraint.scala
@@ -2,6 +2,7 @@ package scalatro
 package model.rng.seed
 
 import model.commons.{Card, HandType, Joker, Planet}
+import model.extra.Hint
 import model.rng.seed.SimulationTypes.SimRound
 
 sealed trait SeedConstraint:
@@ -14,7 +15,11 @@ final case class InitialHandWithCards(cards: Seq[Card], round: Int)
 
 final case class InitialHandWithHandType(handType: HandType, round: Int)
     extends SeedConstraint:
-  def isSatisfiedBy(e: SimRound): Boolean = HandType.detect(e.hand) == handType
+  def isSatisfiedBy(e: SimRound): Boolean =
+    Hint
+      .allPlayableHands(e.hand)
+      .filter(_.sizeIs == 5)
+      .exists(HandType.contains(_, handType))
 
 final case class CardPackContains(card: Card, round: Int)
     extends SeedConstraint:
