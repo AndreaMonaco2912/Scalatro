9e07612dbd68eb56f58bb41617798b2c1cbc8ced
Andrea Samori <andrea.samori3@studio.unibo.it>
2026-07-13 19:08:51 +0200

    refactor: swap to lowercase

--- src/main/scala/model/commons/Pack.scala
+++ src/main/scala/model/commons/Pack.scala
@@ -7,8 +7,7 @@ import model.rng.{ScalatroRng, SelectionPolicy, Weighable}
 case class Pack[A](items: Seq[A])
 
 trait PackFactory[A <: Weighable](using SelectionPolicy[A]):
-  private val SMALL_PACK_SIZE = 3
-  private val BIG_PACK_SIZE = 5
+  private val smallPackSize = 3
   def pool: Pool[A]
   def apply(n: Int)(using rng: ScalatroRng): Pack[A] =
     require(n >= 0, s"cannot present a pack with a negative amount of cards")
@@ -18,13 +17,13 @@ trait PackFactory[A <: Weighable](using SelectionPolicy[A]):
     Pack(rng.draw(pool - Pool(blackList), n))
 
   def smallPack(using rng: ScalatroRng): Pack[A] =
-    Pack(rng.draw(pool, SMALL_PACK_SIZE))
+    Pack(rng.draw(pool, smallPackSize))
   def smallPack(blackList: Seq[A])(using rng: ScalatroRng): Pack[A] =
-    Pack(rng.draw(pool - Pool(blackList), SMALL_PACK_SIZE))
+    Pack(rng.draw(pool - Pool(blackList), smallPackSize))
   def bigPack(using rng: ScalatroRng): Pack[A] =
-    Pack(rng.draw(pool, SMALL_PACK_SIZE))
+    Pack(rng.draw(pool, smallPackSize))
   def bigPack(blackList: Seq[A])(using rng: ScalatroRng): Pack[A] =
-    Pack(rng.draw(pool - Pool(blackList), SMALL_PACK_SIZE))
+    Pack(rng.draw(pool - Pool(blackList), smallPackSize))
 
 class CardsPack(using SelectionPolicy[Card]) extends PackFactory[Card]:
   val pool: Pool[Card] =
