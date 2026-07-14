8fe2efeb20ef6a9fc8734b95f244e82ea0f91ef0
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-07-14 17:03:13 +0200

    docs: update ClickableDeck documentation

--- src/main/scala/view/fx/ClickableDeck.scala
+++ src/main/scala/view/fx/ClickableDeck.scala
@@ -7,6 +7,15 @@ import view.DeckNode
 
 import javafx.scene.layout.Pane
 
+/** A deck node that dispatches [[ManagementAction.ShowDeck]] when clicked.
+  * @param dispatch
+  *   the message dispatch callback
+  */
 class ClickableDeck(dispatch: Msg => Unit):
+  /** Mounts the deck node into the given pane.
+    *
+    * @param into
+    *   the host pane
+    */
   def mount(into: Pane): Unit =
     into.getChildren.add(DeckNode(() => dispatch(ManagementAction.ShowDeck)))
