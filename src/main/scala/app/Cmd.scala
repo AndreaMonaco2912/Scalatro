1d166e8c429b210eec558c1d9fb894e16518873a
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-07-15 17:37:05 +0200

    docs: update Cmd doc

--- src/main/scala/app/Cmd.scala
+++ src/main/scala/app/Cmd.scala
@@ -3,7 +3,21 @@ package app
 
 import model.game.GameState
 
+/** A consequence of a message sent to a certain model that can't be completely
+  * run inside [[Update]].
+  */
 enum Cmd:
+  /** The command performing no effect. */
   case NoOp
+
+  /** Starts a new round from the given game state.
+    * @param gameState
+    *   the game state of the match
+    */
   case Deal(gameState: GameState)
+
+  /** Generates a shop for the given game state.
+    * @param gameState
+    *   the game state of the match
+    */
   case BuildShop(gameState: GameState)
