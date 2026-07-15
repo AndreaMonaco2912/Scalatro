be48e53ec382f6d7815d283518bfaacbf3959792
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-07-15 16:26:11 +0200

    docs: document Dispatcher trait

--- src/main/scala/view/fx/Dispatcher.scala
+++ src/main/scala/view/fx/Dispatcher.scala
@@ -3,7 +3,23 @@ package view.fx
 
 import app.Msg
 
+/** A controller capability for emitting [[Msg]]s to a configurable sink.
+  *
+  * Messages dispatched before a handler is set are silently dropped.
+  */
 trait Dispatcher:
   private var sink: Msg => Unit = _ => ()
+
+  /** Sets the handler that receives dispatched messages.
+    *
+    * @param handler
+    *   the message handler
+    */
   final def onMessage(handler: Msg => Unit): Unit = sink = handler
+
+  /** Sends a message to the configured handler.
+    *
+    * @param msg
+    *   the message to send
+    */
   protected final def dispatch(msg: Msg): Unit = sink(msg)
