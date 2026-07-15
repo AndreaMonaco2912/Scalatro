854d76457ca41c49f02f030985e04bf4060b941f
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-07-15 16:23:20 +0200

    docs: document deck node

--- src/main/scala/view/DeckNode.scala
+++ src/main/scala/view/DeckNode.scala
@@ -4,7 +4,15 @@ package view
 import javafx.geometry.Pos
 import javafx.scene.layout.VBox
 
+/** A factory for the clickable deck node. */
 object DeckNode:
+  /** Creates the deck node.
+    *
+    * @param onClick
+    *   the action invoked when the node is clicked
+    * @return
+    *   the node
+    */
   def apply(onClick: () => Unit): VBox =
     val image = ImageViews(Images.deckBack, 85, 125)
     val box = VBox(6, image)
